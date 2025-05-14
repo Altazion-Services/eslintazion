66e5934a51ceb51cf6c490287ae2c809417743e7
BenoitSafari <gregory.michalak.192@gmail.com>
2025-05-14 14:11:23 +0200

    Vue props and components enforcements

--- modules/rules/rules.vue.mjs
+++ modules/rules/rules.vue.mjs
@@ -1,5 +1,18 @@
 /** @type {Record<string, any>} */
 export const vueRules = {
     'vue/no-v-html': 'off',
-    'vue/html-self-closing': 'off'
+    'vue/html-self-closing': 'off',
+    'vue/define-props-declaration': ['error', 'type-based'],
+    'vue/define-emits-declaration': ['error', 'type-based'],
+    'vue/no-child-content': ['error'],
+    'vue/slot-name-casing': ['error', 'kebab-case'],
+    'vue/custom-event-name-casing': ['error', 'kebab-case'],
+    'vue/component-options-name-casing': ['error', 'kebab-case'],
+    'vue/component-name-in-template-casing': [
+        'error',
+        'kebab-case',
+        {
+            registeredComponentsOnly: false,
+        },
+    ],
 };
